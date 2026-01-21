1fe7ce5f19a83cef6fe582d5f6426da6a93fc475
Uberlan Souza <uberlan-almeida@hotmail.com>
2026-01-21 10:28:55 -0300

    second commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,9 @@ const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Cadastro de Contato",
-  description: "Formulário de cadastro para Pessoa Física e Pessoa Jurídica",
-  generator: "v0.app",
+  title: "Solicitação de Contato",
+  description: "Formulário de solicitação de chamada para Pessoa Física e Pessoa Jurídica",
+  generator: "Boxdesk",
   icons: {
     icon: [
       {
